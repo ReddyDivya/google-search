a80f01aaaee6f7c7311b086ed5716f90c5617444
Divya <divyareddy0205@gmail.com>
2023-05-26 13:27:10 -0700

    google-search-navbar

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+import { BrowserRouter } from 'react-router-dom'
+import {Navbar} from './components/Navbar';
+import {Footer} from './components/Footer';
+import {Routes} from './components/Routes';
+
+const App = () => {
+
+  const [darkTheme, setDarkTheme] = useState(false);//dark/light themes
+
+  return (
+    <div className={darkTheme ? 'dark' : ''}>
+        <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+            <BrowserRouter>
+                <Navbar setDarkTheme={setDarkTheme} darkTheme={darkTheme}/>
+                <Routes/>
+                <Footer/>
+            </BrowserRouter>
+        </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as ReactDOMClient from 'react-dom/client';
+import App from './App';
+import './global.css';
+
+const container = document.getElementById('root');
+
+// Create a root.
+const root = ReactDOMClient.createRoot(container);
+
+// Render an element to the root.
+root.render(<App/>);

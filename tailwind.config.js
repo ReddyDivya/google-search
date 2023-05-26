aa749cfb70c5b6d5803d3b9a7fbf2b94715c1da8
Divya <divyareddy0205@gmail.com>
2023-05-26 16:27:56 -0700

    google-search-dark

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  darkMode: 'class', //enabling darkmode, whenever 'dark' class is present.
   theme: {
     extend: {},
   },

8e7550021835e5407a344f5665fba5850eab7bd4
Divya <divyareddy0205@gmail.com>
2023-05-26 16:47:47 -0700

    google-search-dark

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,7 +4,7 @@ export const Search = () => {
   const [text, setText] = useState('Elon Musk');
 
   return (
-    <div className="relative sm:ml-48 md:ml-72 sm:-mt-10 mt-3">
+    <div className=" sm:ml-48 md:ml-72 sm:-mt-10 mt-3">
       <input value={text} type="text" 
       className="sm:w-96 w-80 h-10 dark:bg-gray-200 border rounded-full shadow-sm outline-none p-6 text-black hover:shadow-lg"
       placeholder="Search Google or type URL"

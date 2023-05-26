113a7584ad305104007ac80b4f8a9a8d864c8bfa
Divya <divyareddy0205@gmail.com>
2023-05-26 16:39:09 -0700

    search

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Navbar} from './components/Navbar';
 import {Footer} from './components/Footer';
 import {Routes} from './components/Routes';
 
+
 const App = () => {
 
   const [darkTheme, setDarkTheme] = useState(false);//dark/light themes
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const Search = () => {
+  const [text, setText] = useState('Elon Musk');
+
   return (
-    <div>
-        <h1>Search</h1>
+    <div className="relative sm:ml-48 md:ml-72 sm:-mt-10 mt-3">
+      <input value={text} type="text" 
+      className="sm:w-96 w-80 h-10 dark:bg-gray-200 border rounded-full shadow-sm outline-none p-6 text-black hover:shadow-lg"
+      placeholder="Search Google or type URL"
+      onChange={(e) => setText(e.target.value)}
+      />
+
     </div>
   )
 }

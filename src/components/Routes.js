3e5a8baed32d842f88f423491cdec8770e09c60c
Divya <divyareddy0205@gmail.com>
2023-05-29 12:32:55 -0700

    routes

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -18,8 +18,12 @@ export const Routes = () => {
             The exact prop is used to match the exact path. 
             We specify the path and the component to render for each route.
 
-            
-
+            In this case, we have three routes:
+              - The root route ("/") renders the Home component.
+              - The "/about" route renders the Videos component.
+              - The Switch component also includes a default route that matches any other paths and renders the NotFound component.
+            -----------------------------------------
+            By using BrowserRouter and defining routes with Route components, we can enable navigation and rendering of different components based on the URL path in our React application.
           */
         }
         <BrowserRouter>

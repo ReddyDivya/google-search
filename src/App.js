99c3305f76a56df7f14a81239ab720240c4e0ccc
Divya <divyareddy0205@gmail.com>
2023-06-01 12:04:13 -0700

    state-context

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import React, {useState} from 'react';
-import { BrowserRouter } from 'react-router-dom'
 import {Navbar} from './components/Navbar';
 import {Footer} from './components/Footer';
 import {Routing} from './components/Routing';
 
 const App = () => {
-  {
-    /*
-      BrowserRouter
-      ------------------
-      We wrap our application components inside the BrowserRouter component.
-      This sets up the router and enables us to use routing functionality in our application.
-      By using BrowserRouter and defining routes with Route components, we can enable navigation and rendering of different components based on the URL path in our React application.
-    */
-  }
+ 
   const [darkTheme, setDarkTheme] = useState(false);//dark/light themes
 
   return (
--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@ import React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
 import App from './App';
 import './global.css';
+import { BrowserRouter as Router} from 'react-router-dom'
 import StateContextProvider from './context/StateContextProvider';
 
+/*
+    BrowserRouter
+    ------------------
+    We wrap our application components inside the BrowserRouter component.
+    This sets up the router and enables us to use routing functionality in our application.
+    By using BrowserRouter and defining routes with Route components, we can enable navigation and rendering of different components based on the URL path in our React application.
+*/
 const container = document.getElementById('root');
 
 // Create a root.
@@ -13,6 +21,9 @@ const root = ReactDOMClient.createRoot(container);
 root.render(
     //entire webApp has access to the state context
     <StateContextProvider>
-        <App/>
+        {/* Router enables us to use the routing functionality*/ }
+        <Router> 
+            <App/>
+        </Router>    
     </StateContextProvider>    
 );

f4c09f356beb87cf81482d5fb63d4eb3f52babdc
Divya <divyareddy0205@gmail.com>
2023-06-01 15:20:15 -0700

    results-component

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,4 +1,16 @@
-import React from 'react'
+import React, {useEffect} from 'react';
+import { useLocation } from 'react-router-dom'; 
+
+/*
+  useLocation
+  -------------------------------
+  - In React Router, the useLocation hook is a built-in hook that allows you to access the current location object.
+  - The location object represents the current URL and provides information about the current route, query parameters, and other location-related data.
+
+  'pathname' represents the path of the current URL.
+  'search' represents the query parameters in the URL.
+  'hash' represents the hash fragment in the URL.
+*/
 
 export const Results = () => {
   return (

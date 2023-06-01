39267167aba1e80148ef367a570d654348e88d01
Divya <divyareddy0205@gmail.com>
2023-06-01 12:25:48 -0700

    links

---
+++ src/components/Links.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom';
+
+//nav links
+const links = [
+    {url : '/search', text: 'All'},
+    {url : '/news', text: 'News'},
+    {url : '/images', text: 'Images'},
+    {url : '/videos', text: 'Videos'},
+]//links
+
+export const Links = () => {
+  return (
+    <div>Links</div>
+  )
+}

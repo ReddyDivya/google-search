d5646d9686e26a35fcf4fbb156ef3528d21acc40
Divya <divyareddy0205@gmail.com>
2023-06-01 11:53:16 -0700

    state-context

--- src/context/StateContextProvider.js
+++ src/context/StateContextProvider.js
@@ -2,3 +2,21 @@
 import React, {createContext, useContext, useState} from 'react';
 
 const StateContext = createContext();
+const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1'; //google search api
+
+export const StateContextProvider = () =>{
+    //result, loading, searchTerm
+    const [results, setResults] = useState([]);//results
+    const [loading, setLoading] = useState(false);//loding
+    const [searchTerm, setSearchTerm] = useState('Elon Musk');//search text
+
+    //fetch results
+    const getResults = async(url) => {
+        setLoading(true); //to show loading icon
+
+        
+    }//getResults
+
+}//StateContextProvider
+
+

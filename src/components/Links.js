537d09915997ec8fd071ce52e73dd6a61fc2955e
Divya <divyareddy0205@gmail.com>
2023-06-01 12:34:26 -0700

    links

--- src/components/Links.js
+++ src/components/Links.js
@@ -9,14 +9,10 @@ const links = [
     {url : '/videos', text: 'Videos'},
 ]//links
 
-export const Links = () => {
-  return (
+export const Links = () => (
     <div className="flex sm:justify-around justify-between items-center mt-4">
-        {
-            links.map(({url, text}) => (
-                <NavLink to={url} activeClassName="text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2">{text}</NavLink>
-            ))
-        }
+      {links.map(({ url, text }) => (
+        <NavLink to={url} activeClassName="text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2">{text}</NavLink>
+      ))}
     </div>
-  )
-}
+);

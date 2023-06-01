4a89bd859809eeed418a2e555220d813d9a8d8c9
Divya <divyareddy0205@gmail.com>
2023-06-01 12:39:52 -0700

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,10 +15,10 @@ export const Navbar = ({setDarkTheme, darkTheme }) => {
           <button type="button" onClick={() => setDarkTheme(!darkTheme)}
             className="text-xl dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-2 py-1 hover:shadow-lg"
           >
-            {darkTheme ? '💡 Light' : '🌙 Dark'}
+            {darkTheme ? '💡' : '🌙'}
           </button>
         </div>
         <Search />
     </div>
-  )
+  );
 }
